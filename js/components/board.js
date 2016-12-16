fd6476753c3b0a684ddee167c02008a5a694d2ab
Aaron <aaronmcavinue@gmail.com>
2016-12-16 22:54:54 +0000

    Add callback props to list and board.

--- js/components/board.js
+++ js/components/board.js
@@ -3,12 +3,20 @@ var ReactDOM = require('react-dom');
 var List = require('./list');
 
 var Board = function(props) {
+    function onAddInputChanged() {
+        console.log('Input changed.');
+    }
+    
+    function onAddSubmit() {
+        console.log('Submitted.');
+    }
+    
     return (
         <div>
             <h1>{props.title}</h1>
-            <List title={props.lists[0]} cards="abc"/>
-            <List title={props.lists[1]} cards="def"/>
-            <List title={props.lists[2]} cards="ghi"/>
+            <List title={props.lists[0]} cards="abc" onAddInputChanged={onAddInputChanged} onAddSubmit={onAddSubmit} />
+            <List title={props.lists[1]} cards="def" onAddInputChanged={onAddInputChanged} onAddSubmit={onAddSubmit} />
+            <List title={props.lists[2]} cards="ghi" onAddInputChanged={onAddInputChanged} onAddSubmit={onAddSubmit} />
         </div>
     );
 };
--- js/components/list.js
+++ js/components/list.js
@@ -3,12 +3,21 @@ var ReactDOM = require('react-dom');
 var Card = require('./card');
 
 var List = function(props) {
+    function formSubmit(e) {
+        e.preventDefault();
+        props.onAddSubmit();
+    }
+    
     return (
         <div>
             <h2>{props.title}</h2>
             <Card text={props.cards}/>
             <Card text={props.cards}/>
             <Card text={props.cards}/>
+            <form onSubmit={formSubmit} >
+                <input type="text" name="text-input" onChange={props.onAddInputChanged} />
+                <input type="submit" name="submit-button" /> 
+            </form>
         </div>
     );
 };
